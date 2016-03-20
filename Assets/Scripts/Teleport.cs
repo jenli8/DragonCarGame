3f911bc61e73bdf1e53ae4d9e021317be1994a34
jennykittyli <lij9@bxscience.edu>
2016-03-20 00:46:30 -0400

    Adds kittens, kitten collection script, and music

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public Transform exit;
+	static Transform last; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if(exit == last ) {
+			return; 
+		}
+		TeleportToExit (other); 
+	}
+
+	void OnTriggerExit ( )
+	{
+		if ( exit == last )
+			last = null;
+	}
+	void TeleportToExit ( Collider other )
+	{
+		last = transform;
+		other.transform.position = exit.transform.position;
+	}
+}
